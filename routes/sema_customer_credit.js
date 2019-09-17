8546aa0819e54a2f09e2991d1d88eb42cb3c798f
Kaamuli Anthony Eriya <kaminto@outlook.com>
2019-09-17 14:03:51 +0300

    api refractor

--- routes/sema_customer_credit.js
+++ routes/sema_customer_credit.js
@@ -11,4 +11,34 @@ router.get('/:customerId', function (req, res) {
 	});
 });
 
+router.post('/', function (req, res, next) {
+    semaLog.info(req.body);
+    req.check('customer_account_id', 'Parameter customer_account_id is missing').exists();
+    req.check('topup', 'Parameter topup is missing').exists();
+    req.check('balance', 'Parameter balance is missing').exists();
+
+    req.getValidationResult().then(function (result) {
+        if (!result.isEmpty()) {
+            const errors = result.array().map(elem => {
+                return elem.msg;
+            });
+            semaLog.error(
+                'CREATE customer_credit: Validation error: ' + errors.toString()
+            );
+            res.status(400).send(errors.toString());
+        } else {
+
+            customer_credit.create(req.body).then(kioskClosingStock => {
+                res.status(200).json(kioskClosingStock);
+            })
+                .catch(Sequelize.ForeignKeyConstraintError, function handleError() {
+                    res.status(400).json({ message: 'Invalid Assignment Error' });
+                })
+                .catch(next);
+        }
+    })
+
+
+});
+
 module.exports = router;
